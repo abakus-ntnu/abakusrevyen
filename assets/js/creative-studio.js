537ca90f124b594e8636ba440c5ec7e5981a71da
annistor <annistor@stud.ntnu.no>
2024-03-04 15:12:15 +0100

    Allmøteinfo ny section

--- assets/js/creative-studio.js
+++ assets/js/creative-studio.js
@@ -47,6 +47,54 @@ function lesMer() {
     }
   }
 
+function lesMer1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("lesMerBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Les mer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Les mindre";
+      moreText.style.display = "inline";
+    }
+  }
+
+function lesMer2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("lesMerBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Les mer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Les mindre";
+      moreText.style.display = "inline";
+    }
+  }
+
+function lesMer3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("lesMerBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Les mer";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Les mindre";
+      moreText.style.display = "inline";
+    }
+  }
+
 function lesMerTopp() {
   var dots = document.getElementById("dotsTopp");
   var moreText = document.getElementById("moreTopp");
