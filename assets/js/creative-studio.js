1ce372d4075f001f98cebac0129a71aae0bc59b9
annistor <annistor@stud.ntnu.no>
2024-03-05 09:20:01 +0100

    Fixes2

--- assets/js/creative-studio.js
+++ assets/js/creative-studio.js
@@ -12,7 +12,7 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */ 
 
-// smooth scroll
+// smooth scroll 
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
