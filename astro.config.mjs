fdbc817b8d26eb5aa9d5f27b53239c83570fa88b
LeonTLam <leon2001@live.no>
2025-01-08 19:00:58 +0100

    Fikset markdown og Astro collection, men flytta co ntent til Source

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     defaultStrategy: "hover",
     prefetchAll: true,
   },
-
+  experimental: { contentLayer: true },
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react()],
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob, file } from "astro/loaders";
 
 const oss = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/content/om_oss"}),
+    loader: glob({ pattern: "**/*.md", base: "./om_oss"}),
     schema: z.object({
         title: z.string(),
         description: z.string(),
