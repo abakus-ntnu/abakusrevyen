b0b335d5dc0a716c2a2951739a3ff9141b1f4107
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-01-09 18:10:34 +0100

    Add grid of revy subgroups

---
+++ src/lib/constants.ts
@@ -0,0 +1,55 @@
+import { Facebook, Instagram, Mail } from "lucide-astro";
+
+export const SOCIALS = [
+  {
+    name: "Facebook",
+    profile: "Abakusrevyen",
+    href: "https://facebook.com/Abakusrevyen",
+    Icon: Facebook,
+  },
+  {
+    name: "Instagram",
+    profile: "abakusrevyen",
+    href: "https://instagram.com/abakusrevyen",
+    Icon: Instagram,
+  },
+  {
+    name: "Mail",
+    profile: "revy@abakus.no",
+    href: "mailto:revy@abakus.no",
+    Icon: Mail,
+  },
+];
+
+export const SUBGROUPS = [
+  {
+    name: "Revybandet",
+  },
+  {
+    name: "Koret",
+  },
+  {
+    name: "Sosialgjengen",
+  },
+  {
+    name: "Scene",
+  },
+  {
+    name: "Teknikk",
+  },
+  {
+    name: "Dans",
+  },
+  {
+    name: "Regi",
+  },
+  {
+    name: "Kostyme",
+  },
+  {
+    name: "Skuespill",
+  },
+  {
+    name: "PR",
+  },
+];
