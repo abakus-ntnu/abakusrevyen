320afaa056f420506b6e3abf70dbee818c36f8a0
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-01-10 13:05:48 +0100

    Upgrade online easter egg

--- src/lib/online.ts
+++ src/lib/online.ts
@@ -1,11 +1,33 @@
 function enableOnlineTheme() {
   localStorage.setItem("onlineThemeEnabled", "true");
   window.document.documentElement.classList.add("online");
+
+  const navbarTitle = window.document.getElementById("navbar-title");
+  if (navbarTitle) {
+    navbarTitle.innerHTML = "Onlinerevyen";
+  }
+
+  const footerTitle = window.document.getElementById("footer-title");
+  if (footerTitle) {
+    footerTitle.innerHTML = "Onlinerevyen";
+  }
+
+  const navbarLogo: HTMLImageElement = window.document.getElementById(
+    "navbar-logo",
+  ) as HTMLImageElement;
+  if (navbarLogo) {
+    navbarLogo.src =
+      "https://wiki.online.ntnu.no/attachments/680-Online_bla_o.png";
+  }
 }
 
 function disableOnlineTheme() {
   localStorage.removeItem("onlineThemeEnabled");
   window.document.documentElement.classList.remove("online");
+
+  // Cant be bothered to fix all the sources and texts manually,
+  // just refresh the page instead. Same thing.
+  window.location.reload();
 }
 
 function onLoad() {
