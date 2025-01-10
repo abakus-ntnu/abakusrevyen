396c11da3a1f7b22222e441a60343a4e4908780b
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-01-10 14:11:09 +0100

    Fix ics file link

--- src/lib/calender.ts
+++ src/lib/calender.ts
@@ -4,6 +4,10 @@ const SHOWS = [
     when: new Date(14, 2, 2025, 20, 0, 0),
     where: "Byscenen i Trondheim",
   },
+  {
+    name: "Forestilling 2",
+    when: new Date(15, 2, 2025, 12, 0, 0),
+  },
 ];
 
 export function getCalenderURL() {
@@ -17,12 +21,15 @@ export function getCalenderURL() {
   let ics = "";
 
   for (let show of SHOWS) {
+    let endtime = new Date(show.when);
+    endtime.setHours(show.when.getHours() + 2);
+
     ics += `BEGIN:VCALENDAR
 VERSION:2.0
 BEGIN:VEVENT
 SUMMARY:${show.name}
 DTSTART:${formatDate(show.when)}
-DTEND:${formatDate(show.when)}
+DTEND:${formatDate(endtime)}
 DTSTAMP:${formatDate(new Date())}
 UID:1736513384666-AbakusrevyenPremiere
 DESCRIPTION:
