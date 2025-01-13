36b228ec0bf0e86c13f968c30710681436ae3890
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-01-13 18:31:04 +0100

    Fix broken calender link

--- src/lib/calender.ts
+++ src/lib/calender.ts
@@ -1,3 +1,4 @@
+import { v4 } from "uuid";
 import { SHOWS } from "./constants";
 
 export function getCalenderURL() {
@@ -8,29 +9,30 @@ export function getCalenderURL() {
       .replace(/\.\d{3}/, "");
   };
 
-  let ics = "";
+  let ics = `BEGIN:VCALENDAR
+VERSION:2.0`;
 
   for (let show of SHOWS) {
     let endtime = new Date(show.when);
     endtime.setHours(show.when.getHours() + 2);
 
-    ics += `BEGIN:VCALENDAR
-VERSION:2.0
+    ics += `
 BEGIN:VEVENT
-SUMMARY:${show.name}
+SUMMARY:Abakusrevyen ${show.name}
 DTSTART:${formatDate(show.when)}
 DTEND:${formatDate(endtime)}
 DTSTAMP:${formatDate(new Date())}
-UID:1736513384666-AbakusrevyenPremiere
+UID:${v4()}-${show.name.replaceAll(" ", "")}
 DESCRIPTION:
 LOCATION:${show.where}
 ORGANIZER:Abakus
 STATUS:CONFIRMED
 PRIORITY:1
 END:VEVENT
-END:VCALENDAR
 `;
   }
+
+  ics += "END:VCALENDAR";
 
   return `data:text/calendar;charset=utf8,${encodeURIComponent(ics)}`;
 }
