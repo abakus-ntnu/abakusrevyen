1e09c9337ba7b1468ab8130c50c5954cf6ef8030
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2025-02-14 14:53:21 +0100

    Modern mobile navbar

--- src/components/Header/MobileNavbar/MobileNavbar.tsx
+++ src/components/Header/MobileNavbar/MobileNavbar.tsx
@@ -1,32 +1,67 @@
-import { useState, type AllHTMLAttributes } from "react";
+import React, {
+  useEffect,
+  useState,
+  type AllHTMLAttributes,
+  type ReactNode,
+} from "react";
+
+import { X } from "lucide-react";
 
 interface Props extends AllHTMLAttributes<HTMLDivElement> {
   items: { [href: string]: string };
 }
 
-export default function MobileNavbar({ items, children }: Readonly<Props>) {
-  const [visible, setVisible] = useState(false);
-
-  const hamburgerButton = (
-    <button onClick={() => setVisible(!visible)}>{children}</button>
-  );
-
+function NavbarSlider({
+  items,
+  onClose,
+}: Readonly<Props & { onClose: () => void }>) {
   const navItems = Object.entries(items).map(([href, title]) => (
-    <li key={href} className="text-lg text-foreground hover:text-accent">
+    <li
+      key={href}
+      className="text-lg text-foreground underline hover:text-accent"
+    >
       <a href={href}>{title}</a>
     </li>
   ));
 
   return (
-    <>
-      <div
-        className={`absolute ${
-          visible ? "" : "hidden"
-        } left-0 top-32 z-50 h-[calc(100vh-128px)] w-screen bg-background`}
-      >
-        <ul className="flex w-full flex-col items-center gap-6">{navItems}</ul>
+    <div className="h-full w-full rounded-l-xl border-l border-bg-accent bg-background px-6">
+      <div className="flex h-32 items-center justify-between">
+        <span
+          id="navbar-title"
+          className="font-serif text-xl font-medium tracking-tight md:text-2xl"
+        >
+          Abakusrevyen
+        </span>
+        <button onClick={() => onClose()}>
+          <X />
+        </button>
       </div>
-      {hamburgerButton}
+      <ul className="items-left flex w-full flex-col gap-8">{navItems}</ul>
+    </div>
+  );
+}
+
+export default function MobileNavbar({ items, children }: Readonly<Props>) {
+  const [isOpen, setOpen] = useState(false);
+
+  function close() {
+    setOpen(false);
+    console.log("Closing");
+  }
+
+  return (
+    <>
+      <button onClick={() => setOpen(!isOpen)}>{children}</button>
+      <div
+        className={`fixed right-0 top-0 z-50 h-screen w-5/6 overflow-hidden ${!isOpen && "pointer-events-none"}`}
+      >
+        <div
+          className={`h-full w-full transition-transform ${!isOpen && "translate-x-full"}`}
+        >
+          <NavbarSlider items={items} onClose={close} />
+        </div>
+      </div>
     </>
   );
 }
